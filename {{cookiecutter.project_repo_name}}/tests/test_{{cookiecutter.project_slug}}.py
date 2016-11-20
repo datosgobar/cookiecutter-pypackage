6b102c10c487dcbc7716b4871014d0b51ef1e4e3
Agustín Benassi <agusbenassi@gmail.com>
2016-11-20 19:31:24 -0300

    Agrego templates de modulos python y su test.

--- {{cookiecutter.project_repo_name}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_repo_name}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,60 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-test_{{ cookiecutter.project_slug }}
-----------------------------------
+"""Tests del modulo {{ cookiecutter.project_slug }}."""
 
-Tests for `{{ cookiecutter.project_slug }}` module.
-"""
-
-{% if cookiecutter.use_pytest == 'y' -%}
-import pytest
-{% else %}
-import sys
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import with_statement
 import unittest
-{%- endif %}
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-from contextlib import contextmanager
-from click.testing import CliRunner
-{%- endif %}
+import nose
 
-from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-from {{ cookiecutter.project_slug }} import cli
-{%- endif %}
+import {{ cookiecutter.project_slug }}
 
 
-{% if cookiecutter.use_pytest == 'y' -%}
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument.
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-def test_command_line_interface():
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert '{{ cookiecutter.project_slug }}.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
-
-{%- endif %}
-{% else %}
-class Test{{ cookiecutter.project_slug|title }}(unittest.TestCase):
+class {{ cookiecutter.project_slug.replace("_", " ").title().replace(" ", "") }}TestCase(unittest.TestCase):
 
     def setUp(self):
         pass
@@ -62,17 +20,8 @@ class Test{{ cookiecutter.project_slug|title }}(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_000_something(self):
+    def test_something(self):
         pass
-{% if cookiecutter.command_line_interface|lower == 'click' %}
-    def test_command_line_interface(self):
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert '{{ cookiecutter.project_slug }}.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
 
-{%- endif %}
-{%- endif %}
+if __name__ == '__main__':
+    nose.run(defaultTest=__name__)
